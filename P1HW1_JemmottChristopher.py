69f581b62908c04d6a486e0abb80533e28d7e3d2
jemmottc3832 <jemmottc3832@student.faytechcc.edu>
2026-06-27 04:55:23 -0400

    Add files via upload

---
+++ P1HW1_JemmottChristopher.py
@@ -0,0 +1,28 @@
+# Christopher Jemmott
+# 6-25-2026
+# Calculating exponents and addition and subtraction
+# calculate exponents
+
+print("------------Exponents------------") \
+
+print()
+
+base = int(input("Enter a base number: "))
+exponent = int(input("Enter a exponent number: "))
+result = base ** exponent
+print(base,"raised to the power of", exponent,"is",result, "!!")
+
+# calculate addition and subtraction 
+print("------------Addition and Subtraction------------") \
+
+print()
+
+num1 = int(input("Enter a starting integer: "))
+num2 = int(input("Enter an integer to add: "))
+num3 = int(input("Enter an integer to subtract: "))
+
+sum_result = num1 + num2
+final_result = sum_result - num3
+
+print(num1, "+", num2,"-", num3, "is equal to", final_result, "!!") 
+
