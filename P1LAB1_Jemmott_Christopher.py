da2b593ee781f76fe86ab5a409dd7483152b1c43
jemmottc3832 <jemmottc3832@student.faytechcc.edu>
2026-06-27 04:54:41 -0400

    Taking input printing output

---
+++ P1LAB1_Jemmott_Christopher.py
@@ -0,0 +1,8 @@
+# christopher jemmott
+# 6-25-2026
+# p1lab1
+# input and print
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print("Hello,", first_name, last_name +"!","Welcome to CTI-110!")
